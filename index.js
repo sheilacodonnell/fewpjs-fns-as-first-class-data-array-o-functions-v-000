00d3e9231ba9cefffcb6faf22393bbbe330c2de3
Sheila O'Donnell <sheila@contexttravel.com>
2020-03-30 13:51:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function wakeDog(dogName, dogBreed) {
-  var dogName = 'Xerox'
-var dogBreed = 'Border Collie'
+  var dogName = 'Xerox';
+  var dogBreed = 'Border Collie';
   console.log(`Wake ${dogName} the ${dogBreed}`);
 }
 
